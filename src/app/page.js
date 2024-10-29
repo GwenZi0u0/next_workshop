8f6da51ea47b64b2f9dde58c8dd34ec758a4e164
GwenZi0u0 <a0918566336@gmail.com>
2024-10-29 10:50:18 +0800

    Feat :  homepage - app/page (Next.js)
    - Create three buttons.
    - Three buttons respectively direct to /ssr, /ssg, and /csr.
    - import Link

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
+      <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
+        <Link href="/csr">
+          <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded hover:bg-blue-600 transition duration-200">
+            Go to CSR Page
+          </button>
+        </Link>
+        <Link href="/ssr">
+          <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded hover:bg-blue-600 transition duration-200">
+            Go to SSR Page
+          </button>
+        </Link>
+        <Link href="/ssg">
+          <button className="bg-blue-500 text-white font-bold py-4 px-6 rounded hover:bg-blue-600 transition duration-200">
+            Go to SSG Page
+          </button>
+        </Link>
+      </main>
+    </div>
+  );
+}
